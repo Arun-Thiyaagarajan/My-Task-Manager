35cd7f104d3f8870074b46ac3c40b4c88d656543
Arun Thiyaagarajan <thiyaagarajan.n@ideaelan.com>
2026-03-14 12:43:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "taskflow-gtchw",
+  "appId": "1:354796818691:web:102ad771b020452f195936",
+  "apiKey": "AIzaSyAjwBCA7uqI6mIkjPVU90Xxcf-_b089if4",
+  "authDomain": "taskflow-gtchw.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "354796818691"
+};
