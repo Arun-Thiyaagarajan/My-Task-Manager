30253cb0a3fcc661f2b78d47409060103bdc17dd
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2026-03-24 16:37:44 +0530

    PR Field check in PDF

--- src/lib/share-utils.ts
+++ src/lib/share-utils.ts
@@ -447,7 +447,9 @@ const _drawTaskOnPage = async (
         }
     });
 
-    if (task.prLinks && Object.keys(task.prLinks).length > 0) {
+    const prField = () => uiConfig?.fields?.find(f => f.key === 'prLinks' && f.isActive);
+
+    if (prField() && task.prLinks && Object.keys(task.prLinks).length > 0) {
         let firstPrLabel = '';
         let firstPrValue: { text: string; link: string } | '' = '';
         outer: for (const [env, repos] of Object.entries(task.prLinks)) {
