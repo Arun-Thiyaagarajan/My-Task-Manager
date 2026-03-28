d855fc96c0745f82c10c5f8b787e55c44307ddc8
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2026-03-28 16:19:13 +0530

    Environment files updated for development and production deployment

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "taskflow-gtchw",
-  "appId": "1:354796818691:web:102ad771b020452f195936",
-  "apiKey": "AIzaSyAjwBCA7uqI6mIkjPVU90Xxcf-_b089if4",
-  "authDomain": "taskflow-gtchw.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "354796818691"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
 };
