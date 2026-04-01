546c130532ca1ee306bdb058d97664733daae6c3
Arun Thiyaagarajan <arunthiyaagarajan.ta@gmail.com>
2026-04-01 21:11:16 +0530

    Clear Filter fix for status group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2098,6 +2098,7 @@ export default function Home() {
                                                 setRepoFilter([]);
                                                 setDeploymentFilter([]);
                                                 setTagsFilter([]);
+                                                setStatusGroupFilter([]);
                                                 setSearchQuery('');
                                                 setExecutedSearchQuery('');
                                             }}
